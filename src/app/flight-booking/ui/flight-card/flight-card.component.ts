4194e5f7470401fde7daba3255646646c7199a8b
michael.zikes <michael.zikes@intauria.com>
2021-12-17 15:35:45 +0100

    Lifecycle Hooks

--- src/app/flight-booking/ui/flight-card/flight-card.component.ts
+++ src/app/flight-booking/ui/flight-card/flight-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Flight } from '../../entities/flight';
 
 @Component({
@@ -6,10 +6,18 @@ import { Flight } from '../../entities/flight';
   templateUrl: './flight-card.component.html',
   styleUrls: ['./flight-card.component.css']
 })
-export class FlightCardComponent {
+export class FlightCardComponent implements OnInit, OnDestroy {
   @Input() item: Flight | undefined;
   @Input() selected: boolean = false;
   @Output() selectedChange = new EventEmitter<boolean>();
+
+  ngOnInit(): void {
+    console.log('FlightCard INIT');
+  }
+
+  ngOnDestroy(): void {
+    console.log('FlightCard DESTROY');
+  }
 
   toggleSelection(): void {
     this.selected = !this.selected;
