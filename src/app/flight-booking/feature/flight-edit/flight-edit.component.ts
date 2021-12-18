f771fafa2c65b48d4ef45693e7ffb34ec76c762c
michael.zikes <michael.zikes@intauria.com>
2021-12-18 11:18:15 +0100

    Reactive Forms

--- src/app/flight-booking/feature/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/feature/flight-edit/flight-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'flight-flight-edit',
@@ -9,15 +11,44 @@ import { ActivatedRoute } from '@angular/router';
 export class FlightEditComponent implements OnInit {
   id: number = 0;
   showDetails: boolean = false;
+  editForm: FormGroup = this.getInitialEditForm();
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder) {
     this.route.paramMap.subscribe(params => {
       this.id = +params.get('id')!;
+      this.editForm.controls.id.setValue(this.id);
       this.showDetails = params.get('showDetails') === 'true';
     });
   }
 
   ngOnInit(): void {
+    this.editForm.valueChanges.pipe(
+      filter(() => this.editForm.valid),
+      debounceTime(300)
+    ).subscribe(console.log);
   }
 
+  getInitialEditForm(): FormGroup {
+    return this.fb.group({
+      id: [0],
+      from: ['Graz', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      to: ['Hamburg', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      date: [new Date().toISOString()]
+    });
+  }
+
+  save(): void {
+    console.log('value', this.editForm.value);
+    console.log('valid', this.editForm.valid);
+    console.log('dirty', this.editForm.dirty);
+    console.log('touched', this.editForm.touched);
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { CityPipe } from './pipes/city.pipe';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StatusPipe } from './pipes/status.pipe';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CityValidatorDirective } from './validation/city-validator.directive';
 
 @NgModule({
@@ -13,13 +13,15 @@ import { CityValidatorDirective } from './validation/city-validator.directive';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     StatusPipe,
     CityPipe,
     CityValidatorDirective,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
 ]
 })
 export class SharedModule { }
